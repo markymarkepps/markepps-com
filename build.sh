8b9918005e9e119b15381f473c251e688a52b0ce
Mark Epps <4727781+markymarkepps@users.noreply.github.com>
2025-10-10 16:29:31 +1100

    feat: updated build.sh to account for staging

--- build.sh
+++ build.sh
@@ -9,8 +9,14 @@ if [ "$CF_PAGES_BRANCH" == "main" ]; then
   # Production build
   echo "Building for production (main branch)..."
   hugo --gc --minify -b https://markepps.com
+
+elif [ "$CF_PAGES_BRANCH" == "staging" ]; then
+  # Staging build
+  echo "Building for staging..."
+  hugo --gc --minify -b https://staging.markepps-com.pages.dev
+
 else
-  # Preview build (for staging and all other branches)
+  # Preview build (for all other branches)
   echo "Building for preview..."
   hugo --gc --minify -b $CF_PAGES_URL
 fi
